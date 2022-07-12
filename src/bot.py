de1b4842cfe6a7dbadbeed8b1d2aad21f7653140
Nikolay Ermolovich <rupreht@rupreht.ru>
2022-07-12 21:37:24 +0500

    -

--- src/bot.py
+++ src/bot.py
@@ -151,8 +151,7 @@ async def cron_fresh_events():
 
 def main() -> None:
     """ Main """
-    loop = asyncio.get_running_loop()
-    loop.create_task(cron_fresh_events())
+    asyncio.get_running_loop().create_task(cron_fresh_events())
     executor.start_polling(dispatcher)
 
 if __name__ == '__main__':
