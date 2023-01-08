1646a1f13e4f17b0014c2acc0020f9294461a31c
Nikolay Ermolovich <rupreht@rupreht.ru>
2023-01-08 21:06:01 +0500

    fix for logs

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -3,7 +3,5 @@
 export STATIC_URL=/static
 export STATIC_PATH=/app/app/static
 
-crond -l 2 -f >/dev/stdout 2>/dev/stderr &
+crond -l 2 -f >/proc/self/fd/1 2>/proc/self/fd/2 &
 uwsgi --http 0.0.0.0:5000 --wsgi-file WSGI.py --callable app --stats 0.0.0.0:5001
-
-# cd /app && python src/bot.py
