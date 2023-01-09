92ae56e1ce39135fe78534fe9323bcd19c46bd8f
Nikolay Ermolovich <rupreht@rupreht.ru>
2023-01-09 23:05:58 +0500

    fix

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -4,4 +4,4 @@ export STATIC_URL=/static
 export STATIC_PATH=/app/app/static
 
 crond -l 2 -f >/proc/self/fd/1 2>/proc/self/fd/2 &
-uwsgi --http 0.0.0.0:5000 --wsgi-file WSGI.py --callable app --uid app --gid app --processes 4 --threads 2 --stats 0.0.0.0:5001
+uwsgi --http :5000 --wsgi-file WSGI.py --callable app --uid app --gid app --processes 4 --threads 2
