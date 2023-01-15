3df1694501b068a3be9fea1ef9794541e15fd9b2
Nikolay Ermolovich <rupreht@rupreht.ru>
2023-01-16 04:35:28 +0500

    add flask db upgrade

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -3,5 +3,7 @@
 export STATIC_URL=/static
 export STATIC_PATH=/app/app/static
 
+cd /app && flask db upgrade
+
 crond -l 2 -f >/proc/self/fd/1 2>/proc/self/fd/2 &
 uwsgi --http :5000 --wsgi-file WSGI.py --callable app --uid app --gid app --processes 4 --threads 2
